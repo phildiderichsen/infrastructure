99a011bb8e4865ebdd1db8de1eb11c92e3811529
Philip Diderichsen <philip.diderichsen@gmail.com>
2020-09-07 16:21:36 +0200

    Opretter setting til MEMOcorpus.

---
+++ korp/setups/MEMOcorpus/backend/start.sh
@@ -0,0 +1,12 @@
+# Run test corpus encoding
+chmod +x /opt/host_corpora/encodingscripts/encode_testcorpus.sh
+/opt/host_corpora/encodingscripts/encode_testcorpus.sh
+
+chmod +x /opt/host_corpora/encodingscripts/encode_MEMOtestcorpus.sh
+/opt/host_corpora/encodingscripts/encode_MEMOtestcorpus.sh
+
+service mysql start && python3 /opt/korp-backend/korp.py
+
+# TODO: recommended server (for later)
+#pip3 install gunicorn
+#gunicorn --worker-class gevent --bind 0.0.0.0:1234 --workers 4 --max-requests 250 --limit-request-line 0 korp:app
